1b0f2236c2ff832cf16a78abcd5c87da262321d4
ravinduf <ravinduf@rocketmail.com>
2021-02-15 13:12:42 +0530

    created PersonController

--- src/main/java/com/senal/test/api/PersonController.java
+++ src/main/java/com/senal/test/api/PersonController.java
@@ -1,4 +1,19 @@
 package com.senal.test.api;
 
+import com.senal.test.model.Person;
+import com.senal.test.service.PersonService;
+
 public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
+    public void addPerson(Person person){
+        personService.addPerson(person);
+    }
+
+    
 }
