46d9d777718d13a226e48fe8e7da1fc5cc501ef3
ravinduf <ravinduf@rocketmail.com>
2021-02-15 10:52:44 +0530

    created basic PersonDao

--- src/main/java/com/senal/test/dao/PersonDao.java
+++ src/main/java/com/senal/test/dao/PersonDao.java
@@ -1,4 +1,16 @@
 package com.senal.test.dao;
 
-public class PersonDao {
+import com.senal.test.model.Person;
+
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int addPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id, person);
+    }
+
 }
