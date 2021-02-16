e3bd11e4f5c0c4c143a82c805456d60e43199534
ravinduf <ravinduf@rocketmail.com>
2021-02-16 12:08:11 +0530

    basic functionality to delete, update, findById added and implemented methods to retrieve personById

--- src/main/java/com/senal/test/dao/FakePersonDataAccessService.java
+++ src/main/java/com/senal/test/dao/FakePersonDataAccessService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository("fakeDao")
@@ -21,6 +22,23 @@ public class FakePersonDataAccessService implements PersonDao {
     @Override
     public List<Person> selectAllPeople() {
         return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
     }
 
 }
